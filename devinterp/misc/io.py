ee09607ea662e3b7fbebf84b70588457d289dce0
Jesse Hoogland <jessequinten@gmail.com>
2023-08-15 13:19:33 +0000

    RFC clean up ActivationProbe

--- devinterp/misc/io.py
+++ devinterp/misc/io.py
@@ -1,310 +1,44 @@
-# Copyright 2018 The Lucid Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Methods for displaying images from Numpy arrays."""
-
-import base64
-import logging
+import functools
 import math
-from io import BytesIO
-from string import Template
+import os
+import random
+from dataclasses import asdict, dataclass, field
+from typing import Callable, Container, Dict, List, Optional, Tuple, TypedDict
 
-import IPython.display
 import numpy as np
-import PIL.Image
-
-# create logger with module name, e.g. lucid.misc.io.array_to_image
-log = logging.getLogger(__name__)
-
-
-# serialize array
-
-def _normalize_array(array: np.ndarray, domain=(0, 1)) -> np.ndarray:
-    array = np.array(array)
-    array = np.squeeze(array)
-    assert len(array.shape) <= 3
-    assert np.issubdtype(array.dtype, np.number)
-    assert not np.isnan(array).any()
-
-    low, high = np.min(array), np.max(array)
-    if domain is None:
-        log.debug("No domain specified, normalizing from measured (~%.2f, ~%.2f)", low, high)
-        domain = (low, high)
-
-    if low < domain[0] or high > domain[1]:
-        log.info("Clipping domain from (~{:.2f}, ~{:.2f}) to (~{:.2f}, ~{:.2f}).".format(low, high, domain[0], domain[1]))
-        array = array.clip(*domain)
-
-    min_value, max_value = np.iinfo(np.uint8).min, np.iinfo(np.uint8).max
-    if np.issubdtype(array.dtype, np.inexact):
-        offset = domain[0]
-        if offset != 0:
-            array -= offset
-            log.debug("Converting inexact array by subtracting -%.2f.", offset)
-        if domain[0] != domain[1]:
-            scalar = max_value / (domain[1] - domain[0])
-            if scalar != 1:
-                array *= scalar
-                log.debug("Converting inexact array by scaling by %.2f.", scalar)
-
-    return array.clip(min_value, max_value).astype(np.uint8)
-
-
-def _serialize_normalized_array(array: np.ndarray, fmt='png', quality=70) -> bytes:
-    assert np.issubdtype(array.dtype, np.unsignedinteger)
-    assert np.max(array) <= np.iinfo(array.dtype).max
-    assert array.shape[-1] > 1
-
-    image = PIL.Image.fromarray(array)
-    image_bytes = BytesIO()
-    image.save(image_bytes, fmt, quality=quality)
-    return image_bytes.getvalue()
-
-
-def serialize_array(array: np.ndarray, domain=(0, 1), fmt='png', quality=70) -> bytes:
-    normalized = _normalize_array(array, domain=domain)
-    return _serialize_normalized_array(normalized, fmt=fmt, quality=quality)
-
-
-JS_ARRAY_TYPES = {'int8', 'int16', 'int32', 'uint8', 'uint16', 'uint32', 'float32', 'float64'}
-
-
-def array_to_jsbuffer(array: np.ndarray) -> str:
-    if array.ndim != 1:
-        raise TypeError('Only 1d arrays can be converted JS TypedArray.')
-    if array.dtype.name not in JS_ARRAY_TYPES:
-        raise TypeError('Array dtype not supported by JS TypedArray.')
-
-    js_type_name = array.dtype.name.capitalize() + 'Array'
-    data_base64 = base64.b64encode(array.tobytes()).decode('ascii')
-    code = f"""
-    (function() {{
-      const data = atob("{data_base64}");
-      const buf = new Uint8Array(data.length);
-      for (var i=0; i<data.length; ++i) {{
-        buf[i] = data.charCodeAt(i);
-      }}
-      var array_type = {js_type_name};
-      if (array_type == Uint8Array) {{
-        return buf;
-      }}
-      return new array_type(buf.buffer);
-    }})()
-    """
-    return code
-
-
-# collapse channels
-
-def hue_to_rgb(ang: float, warp: bool = True) -> np.ndarray:
-    """Produce an RGB unit vector corresponding to a hue of a given angle."""
-    ang = ang - 360 * (ang // 360)
-    colors = np.asarray([
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1],
-        [1, 0, 1],
-    ])
-    colors /= np.linalg.norm(colors, axis=1, keepdims=True)
-    R = 360 / len(colors)
-    n = math.floor(ang / R)
-    D = (ang - n * R) / R
-
-    if warp:
-        adj = lambda x: math.sin(x * math.pi / 2)
-        if n % 2 == 0:
-            D = adj(D)
-        else:
-            D = 1 - adj(1 - D)
-
-    v = (1 - D) * colors[n] + D * colors[(n + 1) % len(colors)]
-    return v / np.linalg.norm(v)
-
-
-def sparse_channels_to_rgb(X: np.ndarray) -> np.ndarray:
-    assert (X >= 0).all()
-
-    K = X.shape[-1]
-    rgb = 0
-
-    for i in range(K):
-        ang = 360 * i / K
-        color = hue_to_rgb(ang)
-        color = color[tuple(None for _ in range(len(X.shape) - 1))]
-        rgb += X[..., i, None] * color
-
-    rgb += np.ones(X.shape[:-1])[..., None] * (X.sum(-1) - X.max(-1))[..., None]
-    rgb /= 1e-4 + np.linalg.norm(rgb, axis=-1, keepdims=True)
-    rgb *= np.linalg.norm(X, axis=-1, keepdims=True)
-
-    return rgb
-
-
-def collapse_channels(X: np.ndarray) -> np.ndarray:
-    if (X < 0).any():
-        X = np.concatenate([np.maximum(0, X), np.maximum(0, -X)], axis=-1)
-    return sparse_channels_to_rgb(X)
-
-
-# showing
-
-
-def _display_html(html_str):
-  IPython.display.display(IPython.display.HTML(html_str))
-
-
-def _image_url(array, fmt='png', mode="data", quality=90, domain=None):
-  """Create a data URL representing an image from a PIL.Image.
-
-  Args:
-    image: a numpy array
-    mode: presently only supports "data" for data URL
-
-  Returns:
-    URL representing image
-  """
-  supported_modes = ("data")
-  if mode not in supported_modes:
-    message = "Unsupported mode '%s', should be one of '%s'."
-    raise ValueError(message, mode, supported_modes)
-
-  image_data = serialize_array(array, fmt=fmt, quality=quality, domain=domain)
-  base64_byte_string = base64.b64encode(image_data).decode('ascii')
-  return "data:image/" + fmt.upper() + ";base64," + base64_byte_string
-
-
-# public functions
-
-def _image_html(array, w=None, domain=None, fmt='png'):
-  url = _image_url(array, domain=domain, fmt=fmt)
-  style = "image-rendering: pixelated; image-rendering: crisp-edges;"
-  if w is not None:
-    style += "width: {w}px;".format(w=w)
-  return """<img src="{url}" style="{style}">""".format(**locals())
-
-def image(array, domain=None, w=None, format='png', **kwargs):
-  """Display an image.
-
-  Args:
-    array: NumPy array representing the image
-    fmt: Image format e.g. png, jpeg
-    domain: Domain of pixel values, inferred from min & max values if None
-    w: width of output image, scaled using nearest neighbor interpolation.
-      size unchanged if None
-  """
-
-  _display_html(
-    _image_html(array, w=w, domain=domain, fmt=format)
-  )
-
-
-def images(arrays, labels=None, domain=None, w=None):
-  """Display a list of images with optional labels.
-
-  Args:
-    arrays: A list of NumPy arrays representing images
-    labels: A list of strings to label each image.
-      Defaults to show index if None
-    domain: Domain of pixel values, inferred from min & max values if None
-    w: width of output image, scaled using nearest neighbor interpolation.
-      size unchanged if None
-  """
-
-  s = '<div style="display: flex; flex-direction: row;">'
-  for i, array in enumerate(arrays):
-    label = labels[i] if labels is not None else i
-    img_html = _image_html(array, w=w, domain=domain)
-    s += """<div style="margin-right:10px; margin-top: 4px;">
-              {label} <br/>
-              {img_html}
-            </div>""".format(**locals())
-  s += "</div>"
-  _display_html(s)
-
-
-def show(thing, domain=(0, 1), **kwargs):
-  """Display a numpy array without having to specify what it represents.
-
-  This module will attempt to infer how to display your tensor based on its
-  rank, shape and dtype. rank 4 tensors will be displayed as image grids, rank
-  2 and 3 tensors as images.
-
-  For tensors of rank 3 or 4, the innermost dimension is interpreted as channel.
-  Depending on the size of that dimension, different types of images will be
-  generated:
-
-    shp[-1]
-      = 1  --  Black and white image.
-      = 2  --  See >4
-      = 3  --  RGB image.
-      = 4  --  RGBA image.
-      > 4  --  Collapse into an RGB image.
-               If all positive: each dimension gets an evenly spaced hue.
-               If pos and neg: each dimension gets two hues
-                  (180 degrees apart) for positive and negative.
-
-  Common optional arguments:
-
-    domain: range values can be between, for displaying normal images
-      None  = infer domain with heuristics
-      (a,b) = clip values to be between a (min) and b (max).
-
-    w: width of displayed images
-      None  = display 1 pixel per value
-      int   = display n pixels per value (often used for small images)
-
-    labels: if displaying multiple objects, label for each object.
-      None  = label with index
-      []    = no labels
-      [...] = label with corresponding list item
-
-  """
-  def collapse_if_needed(arr):
-    K = arr.shape[-1]
-    if K not in [1,3,4]:
-      log.debug("Collapsing %s channels into 3 RGB channels." % K)
-      return collapse_channels(arr)
-    else:
-      return arr
-
-
-  if isinstance(thing, np.ndarray):
-    rank = len(thing.shape)
-
-    if rank in [3,4]:
-      thing = collapse_if_needed(thing)
-
-    if rank == 4:
-      log.debug("Show is assuming rank 4 tensor to be a list of images.")
-      images(thing, domain=domain, **kwargs)
-    elif rank in (2, 3):
-      log.debug("Show is assuming rank 2 or 3 tensor to be an image.")
-      image(thing, domain=domain, **kwargs)
-    else:
-      log.warning("Show only supports numpy arrays of rank 2-4. Using repr().")
-      print(repr(thing))
-  elif isinstance(thing, (list, tuple)):
-    log.debug("Show is assuming list or tuple to be a collection of images.")
-
-    if isinstance(thing[0], np.ndarray) and len(thing[0].shape) == 3:
-      thing = [collapse_if_needed(t) for t in thing]
-
-    images(thing, domain=domain, **kwargs)
-  else:
-    log.warning("Show only supports numpy arrays so far. Using repr().")
-    print(repr(thing))
-
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.utils as vutils
+import wandb
+from matplotlib import pyplot as plt
+from PIL import Image
+from torch import nn
+from torch.optim.lr_scheduler import LambdaLR
+from torchvision import datasets, transforms
+from tqdm.notebook import tqdm
+
+
+def gen_images(*images: torch.Tensor, nrow=None) -> np.ndarray:
+    # Process the optimized input
+    images = [img.detach().cpu().squeeze(0) for img in images]
+    images = [img - img.min() for img in images]
+    images = [img / img.max() for img in images]
+
+    # Create grid
+    grid_image = vutils.make_grid(images, nrow=nrow or len(images))
+
+    # Convert to numpy and transpose for plotting
+    grid_image_np = grid_image.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+
+    return grid_image_np
+
+
+def show_images(*images: torch.Tensor, nrow=None, **kwargs):
+    grid_image_np = gen_images(*images, nrow=nrow)
+
+    # Display using matplotlib
+    plt.figure(**kwargs)  # You can change the size as you prefer
+    plt.imshow(grid_image_np)
+    plt.axis('off') # to remove the axis
+    plt.show()
---
+++ devinterp/viz/activations.py
@@ -0,0 +1,201 @@
+from contextlib import contextmanager
+from typing import Callable, Optional
+
+import torch
+from torch.nn import functional as F
+from torchvision import transforms
+from tqdm import tqdm
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+Transform = Callable[[torch.Tensor], torch.Tensor]
+
+class ActivationProbe:
+    """
+    A utility class to extract the activation value of a specific neuron within a neural network.
+    
+    The location of the target neuron is defined using a string that can specify the layer, channel, and spatial coordinates (y, x). The format allows flexibility in defining the location:
+    
+    - 'layer1.0.conv1.weight.3': Only channel is specified; y and x default to the center (or one-off from center if the width and height are even).
+    - 'layer1.0.conv1.weight.3.2': Channel and y are specified; x defaults to center.
+    - 'layer1.0.conv1.weight.3..2': Channel and x are specified; y defaults to center.
+    - 'layer1.0.conv1.weight.3.2.2': Channel, y, and x are all specified.
+    
+    The class provides methods to register a forward hook into a PyTorch model to capture the activation of the specified neuron during model inference.
+    
+    Attributes:
+        model: The PyTorch model from which to extract the activation.
+        location (str): The dot-separated path specifying the layer, channel, y, and x coordinates.
+        activation: The value of the activation at the specified location.
+        
+    Example:
+        model = ResNet18()
+        extractor = ActivationProbe(model, 'layer1.0.conv1.weight.3')
+        handle = extractor.register_hook()
+        output = model(input_tensor)
+        print(extractor.activation)  # Prints the activation value
+
+    # TODO: Allow wildcards in the location string to extract composite activations (for an entire layer at a time).
+    # TODO: Implement basic arithmetic operators so that you can combine activations (e.g. `0.4 * ActivationProbe(model, 'layer1.0.conv1.weight.3') + 0.6 * ActivationProbe(model, 'layer1.0.conv1.weight.4')`).
+    """
+    
+    def __init__(self, model, location):
+        self.activation = None
+        self.model = model
+        self.location = location.split('.')
+        self.layer_path = []
+        self.channel = None
+        self.y = None
+        self.x = None
+
+        # Split the location into layer path and neuron indices
+        state_dict_keys = list(model.state_dict().keys())
+        for part in self.location:
+            self.layer_path.append(part)
+            path = '.'.join(self.layer_path)
+            
+            if any(key.startswith(path) for key in state_dict_keys):
+                continue
+            else:
+                self.layer_path.pop()
+                self.channel, *yx = map(int, self.location[len(self.layer_path):])
+                if yx:
+                    self.y = yx[0]
+                    if len(yx) > 1:
+                        self.x = yx[1]
+                break
+
+        # Get the target layer
+        self.layer = model
+        for part in self.layer_path[:-1]:
+            self.layer = getattr(self.layer, part)
+
+    def hook_fn(self, module, input, output):
+        y = self.y if self.y is not None else output.size(2) // 2
+        x = self.x if self.x is not None else output.size(3) // 2
+
+        self.activation = output[0, self.channel, y, x]
+
+    def register_hook(self):
+        handle = self.layer.register_forward_hook(self.hook_fn)
+        return handle
+    
+    @contextmanager
+    def watch(self):
+        handle = self.register_hook()
+        yield
+        handle.remove()
+
+    def render(self, transform: Optional[Transform] = None, thresholds: list[int]=[512], verbose: bool = True, seed: int = 0, device: torch.device = DEVICE) -> list[torch.Tensor]:
+        """Renders an image that maximizes the activation of the specified neuron.
+        
+        Args:
+            transform (transforms.Compose, optional): Image transform to apply during optimization.
+            thresholds (list[int], optional): List of iterations at which to save the optimized image.
+            verbose (bool, optional): Whether to print progress information.
+            seed (int, optional): Random seed for initialization of the input image.
+            device (str, optional): Device on which to perform the computation.
+            
+        Returns:
+            tuple[list[torch.Tensor], float]: A tuple containing the final images and the activation value.
+        """
+
+        # Assuming 'model' is your pre-trained ResNet model and 'location' is the string specifying the neuron's location
+        self.model.to(device)
+        self.model.eval()
+
+        with self.watch():
+            # Create a random image (1x3x224x224) to start optimization, with same size as typical ResNet input
+            torch.manual_seed(seed)
+            input_image = torch.rand((1, 3, 32, 32), requires_grad=True, device=device)
+
+            # Optimizer
+            optimizer = torch.optim.Adam([input_image], lr=0.01, weight_decay=1e-3)
+
+            final_images = []
+
+            # Optimization loop
+            pbar = range(max(thresholds) + 1)
+
+            if verbose:
+                pbar = tqdm(pbar, desc=f"Visualizing {self.location} (activation: ???)")
+
+            for iteration in pbar:
+                optimizer.zero_grad()
+                self.model(input_image)  # Forward pass through the model to trigger the hook
+                activation = self.activation
+                loss = -activation  # Maximizing activation
+                loss.backward()
+                optimizer.step()
+
+                if transform:
+                    input_image.data = transform(input_image.data.detach().clone())
+
+                if verbose:
+                    pbar.set_description(f"Visualizing {self.location} (activation: {activation.item():.2f})")
+
+                if iteration in thresholds:
+                    image = input_image.detach().clone()
+                    image = torch.reshape(image, (1, 3, 32, 32))            
+                    final_images.append(image)
+        
+        return final_images
+
+    def render_multiple(self, num_visualizations: int, transform: Transform = None, thresholds: list[int]=[512], verbose: bool = True, seed: int = 0, device: torch.device = DEVICE, diversity_weight: float = 0.1) -> list[list[torch.Tensor]]:
+        """Renders multiple images that maximize the activation of the specified neuron, with a penalty to increase diversity.
+        
+        Args:
+            num_visualizations (int): Number of visualizations to generate.
+            ... other args same as render ...
+            diversity_weight (float, optional): Weight of the diversity penalty.
+        
+        Returns:
+            list[list[torch.Tensor]]: A list containing lists of final images for each visualization.
+        """
+        self.model.to(device)
+        self.model.eval()
+        
+        with self.watch():
+
+            # Create random images (num_visualizations x 3 x 32 x 32) to start optimization
+            torch.manual_seed(seed)
+            input_images = torch.rand((num_visualizations, 3, 32, 32), requires_grad=True, device=device)
+
+            # Optimizer
+            optimizer = torch.optim.Adam([input_images], lr=0.01, weight_decay=1e-3)
+
+            all_final_images = [[] for _ in range(num_visualizations)]
+
+            pbar = range(max(thresholds) + 1)
+            if verbose:
+                pbar = tqdm(pbar, desc=f"Visualizing {self.location} (activation: ???)")
+
+            for iteration in pbar:
+                optimizer.zero_grad()
+                total_loss = torch.tensor(0.0, device=device)
+
+                for i in range(num_visualizations):
+                    self.model(input_images[i].unsqueeze(0))  # Forward pass through the model to trigger the hook
+                    total_loss += -self.activation  # Maximizing activation
+
+                    # Calculate diversity penalty for the current image
+                    for j in range(num_visualizations):
+                        if i != j:
+                            diversity_loss = F.cosine_similarity(input_images[i].view(1, -1), input_images[j].view(1, -1))
+                            total_loss += diversity_weight * diversity_loss
+
+                total_loss.backward()
+                optimizer.step()
+
+                if transform:
+                    input_images.data = transform(input_images.data.detach().clone())
+
+                if verbose:
+                    pbar.set_description(f"Visualizing {self.location} (activation: {-total_loss.item():.2f})")
+
+                if iteration in thresholds:
+                    for i in range(num_visualizations):
+                        image = input_images[i].detach().clone()
+                        image = torch.reshape(image, (3, 32, 32))
+                        all_final_images[i].append(image)
+
+        return all_final_images
