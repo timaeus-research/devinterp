db5e2349ee5e0ec163d7b4c961634281ace40334
George Wang <gwang24@gmail.com>
2023-11-15 09:30:24 -0600

    Add compute_trace_histogram to ChainCallback

--- src/devinterp/slt/callback.py
+++ src/devinterp/slt/callback.py
@@ -1,5 +1,8 @@
 import warnings
 
+import numpy as np
+from typing import Union
+
 
 class ChainCallback:
     def __init__(self, device="cpu"):
@@ -20,7 +23,7 @@ class ChainCallback:
 
         return self
     
-    def compute_trace_statistics(self, attribute_name: str):
+    def _validate_attribute_is_trace(self, attribute_name: str):
         if not self.finalized:
             raise RuntimeError("Cannot compute trace statistics before the callback is finalized.")
         if not hasattr(self, 'num_chains'):
@@ -34,6 +37,9 @@ class ChainCallback:
         if not attribute.shape == (self.num_chains, self.num_draws):
             raise ValueError(f"Attribute {self.attribute} does not have shape {(self.num_chains, self.num_draws)}: {self.attribute} is not a trace.")
 
+    def compute_trace_statistics(self, attribute_name: str):
+        self._validate_attribute_is_trace(attribute_name)
+        attribute = getattr(self, attribute_name)
         return {
             f'{attribute_name}/chain/mean': attribute.mean(axis=1),
             f'{attribute_name}/chain/std': attribute.std(axis=1),
@@ -41,5 +47,17 @@ class ChainCallback:
             f'{attribute_name}/draw/std': attribute.std(axis=0),
         }
     
+    def compute_trace_histograms(self, attribute_name: str, bins: int = None, chain_idx: Union[int, list] = None):
+        self._validate_attribute_is_trace(attribute_name)
+        attribute = getattr(self, attribute_name)
+        if chain_idx is None:
+            chain_idx = list(range(self.num_chains))
+        elif type(chain_idx) == int:
+            chain_idx = [chain_idx]
+        
+        return {
+            f'{attribute_name}/chain/histogram/{i}': np.histogram(attribute[i], bins=bins) for i in chain_idx
+        }
+
     def __call__(self, *args, **kwargs):
         raise NotImplementedError
