1350fa06db82ac7e30888cc7b9d2561559ddf663
Stan van Wingerden <stan@timaeus.co>
2023-11-27 15:25:01 +0000

    ADD dlns

--- tests/slt/sampler_accuracy_test.py
+++ tests/slt/sampler_accuracy_test.py
@@ -50,26 +50,25 @@ def generated_linedot_normalcrossing_dataset():
     return train_loader, train_data, x, y
 
 
-TRUE_LCS = {
-    (0, 1): 1 / 2,
-    (1, 1): 1 / 2,
-    (0, 2): 1 / 4,
-    (1, 2): 1 / 4,
-    (2, 2): 1 / 4,
-    (0, 3): 1 / 6,
-    (1, 3): 1 / 6,
-    (2, 3): 1 / 6,
-    (3, 3): 1 / 6,
+TRUE_LCS_PER_POWER = {
+    ((0, 1), 1 / 2),
+    ((1, 1), 1 / 2),
+    ((0, 2), 1 / 4),
+    ((1, 2), 1 / 4),
+    ((2, 2), 1 / 4),
+    ((0, 3), 1 / 6),
+    ((1, 3), 1 / 6),
+    ((2, 3), 1 / 6),
+    ((3, 3), 1 / 6),
 }
 
 
 @pytest.mark.parametrize("sampling_method", [SGLD, SGNHT])
-@pytest.mark.parametrize("powers", list(TRUE_LCS.keys()))
+@pytest.mark.parametrize("powers, true_lc", TRUE_LCS_PER_POWER)
 def test_accuracy_linedot_normalcrossing(
-    generated_linedot_normalcrossing_dataset, sampling_method, powers
+    generated_linedot_normalcrossing_dataset, sampling_method, powers, true_lc
 ):
     seed = 42
-    true_lc = TRUE_LCS[powers]
     model = Polynomial(powers)
     train_loader, train_data, _, _ = generated_linedot_normalcrossing_dataset
     criterion = F.mse_loss
@@ -98,74 +97,61 @@ def test_accuracy_linedot_normalcrossing(
     llc_mean = llc_estimator.sample()["llc/mean"]
     llc_std_dev = llc_estimator.sample()["llc/std"]
     assert (
-        llc_mean - llc_std_dev < true_lc < llc_mean + llc_std_dev
-    ), f"LLC mean {llc_mean:.3f} +- {llc_std_dev:.3f} does not contain true value {true_lc:.3f} for powers {powers} using {sampling_method}"
+        llc_mean - 2 * llc_std_dev < true_lc < llc_mean + 2 * llc_std_dev
+    ), f"LLC mean {llc_mean:.3f} +- {2*llc_std_dev:.3f} does not contain true value {true_lc:.3f} for powers {powers} using {sampling_method}"
 
 
-# @pytest.fixture
-def generated_rrr_dataset():
-    # Sample data (replace these with your actual data)
+def generated_rrr_dataset(m, n):
     torch.manual_seed(42)
     np.random.seed(42)
     num_samples = 1000
     batch_size = num_samples
-    d_in = 5
-    d_out = 5
-    x = torch.randn(num_samples, d_in)
-    y = torch.randn(num_samples, d_out)
+    x = torch.randn(num_samples, m)
+    y = torch.randn(num_samples, n)
     train_data = TensorDataset(x, y)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     return train_loader, train_data, x, y
 
 
 class ReducedRankRegressor(nn.Module):
-    def __init__(self, input_dim, output_dim, rank):
+    def __init__(self, m, h, n):
         super(ReducedRankRegressor, self).__init__()
-        self.fc1 = nn.Linear(input_dim, rank, bias=False)
-        self.fc2 = nn.Linear(rank, output_dim, bias=False)
+        self.fc1 = nn.Linear(m, h, bias=False)
+        self.fc2 = nn.Linear(h, n, bias=False)
 
     def forward(self, x):
         x = self.fc1(x)
         return self.fc2(x)
 
 
-def test_accuracy_rrr(generated_rrr_dataset):
+@pytest.mark.parametrize("sampling_method", [SGLD, SGNHT])
+@pytest.mark.parametrize(
+    "m,h,n", [(5, 3, 5), (5, 4, 5), (4, 3, 8), (8, 3, 4), (3, 8, 4)]
+)
+def test_accuracy_rrr(sampling_method, m, h, n):
+    # see "The Generalization Error of Reduced Rank Regression in Bayesian Estimation, M. Aoyagi & S. Watanabe, 2004.
     torch.manual_seed(42)
     np.random.seed(42)
     criterion = F.mse_loss
-    rank = 3
-    train_loader, train_data, x, y = generated_rrr_dataset
-    #  M -> H (rank) -> N
-    M = x.size(1)
-    H = rank
-    N = y.size(1)
-    print(M, N, H)
-    print(M + H < N)  # case 2
-    print(N + H < M)  # case 3
-    print(M + N < H)  # case 4
-    print((M + H + N) % 2 == 0)  # case 1, even
-    print((M + H + N) % 2 == 1)  # case 1, odd
-    lambda_case_2 = M * H / 2
-    lambda_case_3 = H * N / 2
-    lambda_case_4 = M * N / 2
-    lambda_if_even = (2 * M * N + 2 * H * N + 2 * M * H - N**2 - M**2 - H**2) / 8
-    lambda_if_odd = (
-        1 + 2 * M * N + 2 * H * N + 2 * M * H - N**2 - M**2 - H**2
-    ) / 8
-    print(lambda_case_2, lambda_case_3, lambda_case_4, lambda_if_even, lambda_if_odd)
-    model = ReducedRankRegressor(M, N, H)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
-    num_samples = len(train_loader)
-    batch_size = num_samples
-    num_chains = 20
+    train_loader, train_data, x, y = generated_rrr_dataset(m, n)
+    #  m -> h (rank) -> n
+    m = x.size(1)
+    n = y.size(1)
+    model = ReducedRankRegressor(m, h, n)
+    case_1_even = (m + h + n) % 2 == 0
+    case_1_odd = (m + h + n) % 2 == 1
+    case_2 = m + h < n
+    case_3 = n + h < m
+    case_4 = m + n < h
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    num_chains = 10
     num_draws = 1_000
-    for epoch in range(25000):
+    for _ in range(2500):
         optimizer.zero_grad()
         outputs = model(x)
         loss = criterion(outputs, y)
         loss.backward()
         optimizer.step()
-    print(loss)
     llc_estimator = LLCEstimator(
         num_chains=num_chains, num_draws=num_draws, n=len(train_data)
     )
@@ -174,11 +160,11 @@ def test_accuracy_rrr(generated_rrr_dataset):
         train_loader,
         criterion=criterion,
         optimizer_kwargs=dict(
-            lr=0.005,
+            lr=0.0005,
             bounding_box_size=2.0,
             num_samples=len(train_data),
         ),
-        sampling_method=SGLD,
+        sampling_method=sampling_method,
         num_chains=num_chains,
         num_draws=num_draws,
         callbacks=[llc_estimator],
@@ -187,7 +173,21 @@ def test_accuracy_rrr(generated_rrr_dataset):
     )
     llc_mean = llc_estimator.sample()["llc/mean"]
     llc_std_dev = llc_estimator.sample()["llc/std"]
-    print(llc_mean, llc_std_dev)
-
-
-test_accuracy_rrr(generated_rrr_dataset())
+    if case_2:
+        case = "2"
+        true_lc = m * h / 2
+    elif case_3:
+        case = "3"
+        true_lc = h * n / 2
+    elif case_4:
+        case = "4"
+        true_lc = m * n / 2
+    elif case_1_even:
+        case = "1_even"
+        true_lc = (2 * m * n + 2 * h * n + 2 * m * h - n**2 - m**2 - h**2) / 8
+    elif case_1_odd:
+        case = "1_odd"
+        true_lc = (1 + 2 * m * n + 2 * h * n + 2 * m * h - n**2 - m**2 - h**2) / 8
+    assert (
+        llc_mean - 2 * llc_std_dev < true_lc < llc_mean + 2 * llc_std_dev
+    ), f"DLN case {case} LLC mean {llc_mean:.3f} +- {2*llc_std_dev:.3f} does not contain true value {true_lc:.3f} for (M, H, N)={(m, h, n)} using {sampling_method}"
