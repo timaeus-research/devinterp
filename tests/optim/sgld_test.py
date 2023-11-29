ef69e4e52475e9431d97d1f1106784cbd7b9354d
Stan van Wingerden <stan@timaeus.co>
2023-11-29 11:56:36 +0000

    CHG formatting

--- tests/optim/sgld_test.py
+++ tests/optim/sgld_test.py
@@ -1,38 +1,39 @@
 from copy import deepcopy
-
 import pytest
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from devinterp.optim.sgld import SGLD  
+from devinterp.optim.sgld import SGLD
 
 
-def test_SGLD_vs_SGD():
-    for lr in [1e-1, 1e-2, 1e-3, 1e-4]:
-        model1 = nn.Linear(1, 1)
-        optimizer_sgd = optim.SGD(model1.parameters(), lr=lr)
-        data = torch.tensor([[1.0]]).reshape(-1, 1)
-        target = torch.tensor([[2.0]]).reshape(-1, 1)
-        
-        model2 = deepcopy(model1)
-        optimizer_sgld = SGLD(model2.parameters(), lr=2*lr, noise_level=0., elasticity=0., temperature=1.)
+@pytest.mark.parametrize("lr", [1e-1, 1e-2, 1e-3, 1e-4])
+def test_SGLD_vs_SGD(lr):
+    model1 = nn.Linear(1, 1)
+    optimizer_sgd = optim.SGD(model1.parameters(), lr=lr)
+    data = torch.tensor([[1.0]]).reshape(-1, 1)
+    target = torch.tensor([[2.0]]).reshape(-1, 1)
 
-        criterion = nn.MSELoss()
+    model2 = deepcopy(model1)
+    optimizer_sgld = SGLD(
+        model2.parameters(), lr=2 * lr, noise_level=0.0, elasticity=0.0, temperature=1.0
+    )
 
-        # Using SGD optimizer
-        optimizer_sgd.zero_grad()
-        output = model1(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer_sgd.step()
+    criterion = nn.MSELoss()
 
-        # Using SGLD optimizer with noise=0, elasticity=0
-        optimizer_sgld.zero_grad()
-        output = model2(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer_sgld.step()
+    # Using SGD optimizer
+    optimizer_sgd.zero_grad()
+    output = model1(data)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer_sgd.step()
 
-        for p1, p2 in zip(model1.parameters(), model2.parameters()):
-            assert torch.allclose(p1, p2, atol=1e-7), f"Parameters differ: {p1} vs {p2}"
+    # Using SGLD optimizer with noise=0, elasticity=0
+    optimizer_sgld.zero_grad()
+    output = model2(data)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer_sgld.step()
+
+    for p1, p2 in zip(model1.parameters(), model2.parameters()):
+        assert torch.allclose(p1, p2, atol=1e-7), f"Parameters differ: {p1} vs {p2}"
--- tests/slt/cov_test.py
+++ tests/slt/cov_test.py
@@ -4,9 +4,11 @@ import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt
 
-from devinterp.slt.cov import (BetweenLayerCovarianceAccumulator,
-                               CovarianceAccumulator,
-                               WithinHeadCovarianceAccumulator)
+from devinterp.slt.cov import (
+    BetweenLayerCovarianceAccumulator,
+    CovarianceAccumulator,
+    WithinHeadCovarianceAccumulator,
+)
 
 
 # A simple dummy model
@@ -16,11 +18,14 @@ class DummyModel(nn.Module):
         self.fc1 = nn.Linear(size, size)
         self.fc2 = nn.Linear(size, size)
 
+
 def accessor_layer1(model: nn.Module):
     return model.fc1.weight.data
 
+
 def accessor_layer2(model: nn.Module):
     return model.fc2.weight.data
+
 
 @pytest.fixture
 def dummy_model():
@@ -42,36 +47,39 @@ def random_vars(cov_matrix):
 @pytest.fixture
 def known_accessor(random_vars):
     i = -1
+
     def _known_accessor(model):
-        nonlocal i 
+        nonlocal i
         i += 1
         return torch.tensor(random_vars[i], dtype=torch.float32)
-    
+
     return _known_accessor
 
 
 def test_covariance_accumulator_with_known_cov(dummy_model, known_accessor, cov_matrix):
     acc = CovarianceAccumulator(num_weights=2, accessors=[known_accessor])
-    
+
     for _ in range(100):
         acc(dummy_model)
-    
+
     acc.finalize()
 
     cov_matrix = acc.to_matrix().detach().cpu().numpy()
-    assert np.allclose(cov_matrix, cov_matrix, atol=1e-1)  # Tolerance due to finite sample
+    assert np.allclose(
+        cov_matrix, cov_matrix, atol=1e-1
+    )  # Tolerance due to finite sample
 
     # Test the eigenvalues
     evals, evecs = np.linalg.eig(cov_matrix)
     eigenspectrum = acc.to_eigen(include_matrix=True)
 
-    assert np.allclose(evals, eigenspectrum['evals'], atol=1e-1)
-    assert np.allclose(np.abs(evecs), np.abs(eigenspectrum['evecs']), atol=1e-1)
+    assert np.allclose(evals, eigenspectrum["evals"], atol=1e-1)
+    assert np.allclose(np.abs(evecs), np.abs(eigenspectrum["evecs"]), atol=1e-1)
 
 
 def test_reset_functionality(dummy_model, known_accessor):
     acc = CovarianceAccumulator(num_weights=2, accessors=[known_accessor])
-    
+
     for _ in range(100):
         acc(dummy_model)
 
@@ -93,11 +101,17 @@ def test_full_model(dummy_model):
     cov_matrix[10:, :10] *= 0.1
 
     def update_model(model):
-        new_params = np.random.multivariate_normal(np.zeros(20), cov_matrix) 
-        model.fc1.weight.data = torch.tensor(new_params[:10].reshape((10,)), dtype=torch.float32)
-        model.fc2.weight.data = torch.tensor(new_params[10:].reshape((10,)), dtype=torch.float32)
+        new_params = np.random.multivariate_normal(np.zeros(20), cov_matrix)
+        model.fc1.weight.data = torch.tensor(
+            new_params[:10].reshape((10,)), dtype=torch.float32
+        )
+        model.fc2.weight.data = torch.tensor(
+            new_params[10:].reshape((10,)), dtype=torch.float32
+        )
 
-    acc = CovarianceAccumulator(num_weights=20, accessors=[accessor_layer1, accessor_layer2])
+    acc = CovarianceAccumulator(
+        num_weights=20, accessors=[accessor_layer1, accessor_layer2]
+    )
 
     for _ in range(100):
         update_model(dummy_model)
@@ -106,22 +120,26 @@ def test_full_model(dummy_model):
     acc.finalize()
 
     cov_matrix = acc.to_matrix().detach().cpu().numpy()
-    assert np.allclose(cov_matrix, cov_matrix, atol=1e-1)  # Tolerance due to finite sample
-            
+    assert np.allclose(
+        cov_matrix, cov_matrix, atol=1e-1
+    )  # Tolerance due to finite sample
 
 
 class DummyTransformer(nn.Module):
     def __init__(self, num_layers=2, num_heads=2, embed_dim=4):
         super().__init__()
-        self.layers = nn.ModuleList([
-            nn.MultiheadAttention(embed_dim, num_heads, bias=False)
-            for _ in range(num_layers)
-        ])
-    
+        self.layers = nn.ModuleList(
+            [
+                nn.MultiheadAttention(embed_dim, num_heads, bias=False)
+                for _ in range(num_layers)
+            ]
+        )
+
     def get_qkv_matrices(self, layer_idx, head_idx):
-        layer = self.layers[layer_idx].in_proj_weight # (3 * embed_dim, embed_dim)
-        head = layer[head_idx*6:(head_idx+1)*6, :]
+        layer = self.layers[layer_idx].in_proj_weight  # (3 * embed_dim, embed_dim)
+        head = layer[head_idx * 6 : (head_idx + 1) * 6, :]
         return head
+
 
 @pytest.fixture
 def dummy_transformer():
@@ -154,24 +172,40 @@ def l2h2(model):
 
 def test_within_head_covariance(dummy_transformer):
     np.random.seed(0)
-    
+
     num_heads = 2
-    num_weights_per_head = 3 * 4 * (4 // num_heads) 
+    num_weights_per_head = 3 * 4 * (4 // num_heads)
     num_parameters_per_layer = num_weights_per_head * num_heads
 
-    cov_matrix_layer_1 = np.random.normal(size=(num_parameters_per_layer, num_parameters_per_layer))
+    cov_matrix_layer_1 = np.random.normal(
+        size=(num_parameters_per_layer, num_parameters_per_layer)
+    )
     cov_matrix_layer_1 = cov_matrix_layer_1 @ cov_matrix_layer_1.T
-    cov_matrix_layer_2 = np.random.normal(size=(num_parameters_per_layer, num_parameters_per_layer))
+    cov_matrix_layer_2 = np.random.normal(
+        size=(num_parameters_per_layer, num_parameters_per_layer)
+    )
     cov_matrix_layer_2 = cov_matrix_layer_2 @ cov_matrix_layer_2.T
 
     def update_model(model):
-        new_params_layer_1 = np.random.multivariate_normal(np.zeros(num_parameters_per_layer), cov_matrix_layer_1)
-        new_params_layer_2 = np.random.multivariate_normal(np.zeros(num_parameters_per_layer), cov_matrix_layer_2)
+        new_params_layer_1 = np.random.multivariate_normal(
+            np.zeros(num_parameters_per_layer), cov_matrix_layer_1
+        )
+        new_params_layer_2 = np.random.multivariate_normal(
+            np.zeros(num_parameters_per_layer), cov_matrix_layer_2
+        )
 
-        model.layers[0].in_proj_weight.data = torch.tensor(new_params_layer_1, dtype=torch.float32).reshape(model.layers[0].in_proj_weight.shape)
-        model.layers[1].in_proj_weight.data = torch.tensor(new_params_layer_2, dtype=torch.float32).reshape(model.layers[1].in_proj_weight.shape)
+        model.layers[0].in_proj_weight.data = torch.tensor(
+            new_params_layer_1, dtype=torch.float32
+        ).reshape(model.layers[0].in_proj_weight.shape)
+        model.layers[1].in_proj_weight.data = torch.tensor(
+            new_params_layer_2, dtype=torch.float32
+        ).reshape(model.layers[1].in_proj_weight.shape)
 
-    acc = WithinHeadCovarianceAccumulator(num_heads, num_weights_per_head, [accessor_transformer_layer1, accessor_transformer_layer2])
+    acc = WithinHeadCovarianceAccumulator(
+        num_heads,
+        num_weights_per_head,
+        [accessor_transformer_layer1, accessor_transformer_layer2],
+    )
 
     for _ in range(1_000):
         update_model(dummy_transformer)
@@ -183,14 +217,25 @@ def test_within_head_covariance(dummy_transformer):
     mse = 0
 
     # Extract submatrices for each layer and head, and validate
-    for l, actual_cov_matrix in enumerate([cov_matrix_layer_1, cov_matrix_layer_2]):  # 2 layers
+    for l, actual_cov_matrix in enumerate(
+        [cov_matrix_layer_1, cov_matrix_layer_2]
+    ):  # 2 layers
         for h in range(num_heads):  # 2 heads
             local_cov_matrix = cov_matrix[l, h]
-            actual_local_cov_matrix = actual_cov_matrix[h*num_weights_per_head:(h+1)*num_weights_per_head, h*num_weights_per_head:(h+1)*num_weights_per_head]
-            local_mse = np.sum((local_cov_matrix - actual_local_cov_matrix) / local_cov_matrix.shape[0]) ** 2
+            actual_local_cov_matrix = actual_cov_matrix[
+                h * num_weights_per_head : (h + 1) * num_weights_per_head,
+                h * num_weights_per_head : (h + 1) * num_weights_per_head,
+            ]
+            local_mse = (
+                np.sum(
+                    (local_cov_matrix - actual_local_cov_matrix)
+                    / local_cov_matrix.shape[0]
+                )
+                ** 2
+            )
 
-            # fig, axes = plt.subplots(1, 2)         
-            # fig.suptitle(f"Layer {l}, Head {h} MSE: {local_mse}")   
+            # fig, axes = plt.subplots(1, 2)
+            # fig.suptitle(f"Layer {l}, Head {h} MSE: {local_mse}")
             # axes[0].imshow(local_cov_matrix)
             # axes[0].set_title("Observed")
             # axes[1].imshow(actual_local_cov_matrix)
@@ -198,31 +243,61 @@ def test_within_head_covariance(dummy_transformer):
             # plt.show()
 
             mse += local_mse / (num_heads * 2)
-    
-    assert mse < 3, f"MSE: {mse}"  # Visually this looks good, but the MSE is a bit high. 
+
+    assert (
+        mse < 3
+    ), f"MSE: {mse}"  # Visually this looks good, but the MSE is a bit high.
 
 
 def test_between_layer_covariance_within_heads(dummy_transformer):
     np.random.seed(0)
-    
+
     num_heads = 2
-    num_weights_per_head = 3 * 4 * (4 // num_heads) 
+    num_weights_per_head = 3 * 4 * (4 // num_heads)
     num_parameters_per_layer = num_weights_per_head * num_heads
 
-    cov_matrix_layer_1 = np.random.normal(size=(num_parameters_per_layer, num_parameters_per_layer))
+    cov_matrix_layer_1 = np.random.normal(
+        size=(num_parameters_per_layer, num_parameters_per_layer)
+    )
     cov_matrix_layer_1 = cov_matrix_layer_1 @ cov_matrix_layer_1.T
-    cov_matrix_layer_2 = np.random.normal(size=(num_parameters_per_layer, num_parameters_per_layer))
+    cov_matrix_layer_2 = np.random.normal(
+        size=(num_parameters_per_layer, num_parameters_per_layer)
+    )
     cov_matrix_layer_2 = cov_matrix_layer_2 @ cov_matrix_layer_2.T
 
     def update_model(model):
-        new_params_layer_1 = np.random.multivariate_normal(np.zeros(num_parameters_per_layer), cov_matrix_layer_1)
-        new_params_layer_2 = np.random.multivariate_normal(np.zeros(num_parameters_per_layer), cov_matrix_layer_2)
+        new_params_layer_1 = np.random.multivariate_normal(
+            np.zeros(num_parameters_per_layer), cov_matrix_layer_1
+        )
+        new_params_layer_2 = np.random.multivariate_normal(
+            np.zeros(num_parameters_per_layer), cov_matrix_layer_2
+        )
 
-        model.layers[0].in_proj_weight.data = torch.tensor(new_params_layer_1, dtype=torch.float32).reshape(model.layers[0].in_proj_weight.shape)
-        model.layers[1].in_proj_weight.data = torch.tensor(new_params_layer_2, dtype=torch.float32).reshape(model.layers[1].in_proj_weight.shape)
+        model.layers[0].in_proj_weight.data = torch.tensor(
+            new_params_layer_1, dtype=torch.float32
+        ).reshape(model.layers[0].in_proj_weight.shape)
+        model.layers[1].in_proj_weight.data = torch.tensor(
+            new_params_layer_2, dtype=torch.float32
+        ).reshape(model.layers[1].in_proj_weight.shape)
 
-    acc1 = WithinHeadCovarianceAccumulator(num_heads, num_weights_per_head, [accessor_transformer_layer1, accessor_transformer_layer2])
-    acc2 = BetweenLayerCovarianceAccumulator(dummy_transformer, pairs={"l1h1": ("l1h1", "l1h1"), "l1h2": ("l1h2", "l1h2"), "l2h1": ("l2h1", "l2h1"), "l2h2": ("l2h2", "l2h2")}, l1h1=l1h1, l1h2=l1h2, l2h1=l2h1, l2h2=l2h2)
+    acc1 = WithinHeadCovarianceAccumulator(
+        num_heads,
+        num_weights_per_head,
+        [accessor_transformer_layer1, accessor_transformer_layer2],
+    )
+    acc2 = BetweenLayerCovarianceAccumulator(
+        dummy_transformer,
+        pairs={
+            "l1h1": ("l1h1", "l1h1"),
+            "l1h2": ("l1h2", "l1h2"),
+            "l2h1": ("l2h1", "l2h1"),
+            "l2h2": ("l2h2", "l2h2"),
+        },
+        l1h1=l1h1,
+        l1h2=l1h2,
+        l2h1=l2h1,
+        l2h2=l2h2,
+    )
 
     for _ in range(1_000):
         update_model(dummy_transformer)
@@ -236,6 +311,6 @@ def test_between_layer_covariance_within_heads(dummy_transformer):
     cov2 = {k: v.detach().cpu().numpy() for k, v in acc2.to_matrices().items()}
 
     # Extract submatrices for each layer and head, and validate
-    for l in range(2):  
-        for h in range(num_heads):  
+    for l in range(2):
+        for h in range(num_heads):
             assert np.allclose(cov1[l, h], cov2[f"l{l+1}h{h+1}"])
