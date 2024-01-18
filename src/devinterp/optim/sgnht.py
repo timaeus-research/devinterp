85d7c196db2445486e02d73179abae69280b877b
svwingerden <stanvanwingerden@gmail.com>
2024-01-18 16:00:57 +0000

    ADD sgnht docstring

--- src/devinterp/optim/sgnht.py
+++ src/devinterp/optim/sgnht.py
@@ -6,23 +6,42 @@ class SGNHT(torch.optim.Optimizer):
     def __init__(
         self,
         params,
-        lr=1e-3,
+        lr=0.01,
         diffusion_factor=0.01,
         bounding_box_size=None,
         num_samples=1,
     ):
-        """
-        Initialize the SGNHT Optimizer.
+        r"""
+        Initialize the Stochastic Gradient Nose Hoover Thermostat (SGNHT) Optimizer.
+        This optimizer blends SGD with an adaptive thermostat variable to control the magnitude of the injected noise,
+        maintaining the kinetic energy of the system.
 
-        :param params: Iterable of parameters to optimize or dicts defining parameter groups
-        :param lr: Learning rate (required)
+        It follows (Ding et al.'s (2014) implementation.
+
+        The equations for the update are as follows:
+
+        $$
+        \Delta w_t = \epsilon\left(\frac{\beta n}{m} \sum_{i=1}^m \nabla \log p\left(y_{l_i} \mid x_{l_i}, w_t\right) - \xi_t w_t \right) + \sqrt{2A} N(0, \epsilon)
+        $$
+        $$
+        \Delta\xi_{t} = \epsilon \left( \frac{1}{n} \| w_t \|^2 - 1 \right)
+        $$
+
+        where $w_t$ is the weight at time $t$, $\epsilon$ is the learning rate,
+        $(\beta n)$ is the inverse temperature (we're in the tempered Bayes paradigm),
+        $n$ is the number of training samples, $m$ is the batch size,
+        $\xi_t$ is the thermostat variable at time $t$, $A$ is the diffusion factor,
+        and $N(0, A)$ represents Gaussian noise with mean 0 and variance $A$.
+
+        :param params: Iterable of parameters to optimize or dicts defining parameter groups (required)
+        :param lr: Learning rate
         :param diffusion_factor: The diffusion factor of the thermostat (default: 0.01)
         :param bounding_box_size: the size of the bounding box enclosing our trajectory The diffusion factor (default: None)
         :param num_samples: Number of samples to average over (default: 1)
         """
         defaults = dict(
             lr=lr,
-            diffusion_factor=0.01,
+            diffusion_factor=diffusion_factor,
             bounding_box_size=bounding_box_size,
             num_samples=num_samples,
         )
@@ -68,7 +87,9 @@ class SGNHT(torch.optim.Optimizer):
                     noise = torch.normal(
                         mean=0.0, std=1.0, size=momentum.size(), device=momentum.device
                     )
-                    momentum.add_(noise * ((group["lr"] * 2 * group["diffusion_factor"]) ** 0.5))
+                    momentum.add_(
+                        noise * ((group["lr"] * 2 * group["diffusion_factor"]) ** 0.5)
+                    )
 
                     # Update position
                     p.data.add_(momentum)
@@ -89,8 +110,10 @@ class SGNHT(torch.optim.Optimizer):
                         ) - 1
                         torch.clamp_(
                             p.data,
-                            min=param_state["initial_param"] - group["bounding_box_size"],
-                            max=param_state["initial_param"] + group["bounding_box_size"],
+                            min=param_state["initial_param"]
+                            - group["bounding_box_size"],
+                            max=param_state["initial_param"]
+                            + group["bounding_box_size"],
                         )
                         momentum.mul_(reflection_coefs)
 
