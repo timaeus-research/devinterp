867976d4b34d5923978c96b88285930b9af92d0c
wz-ml <willzhou97@ucla.edu>
2024-09-06 13:37:13 -0700

    Fix test_sampling.py and annotate (with pytest.mark).

--- tests/integration/test_sampling.py
+++ tests/integration/test_sampling.py
@@ -45,7 +45,7 @@ def get_stats(device, gpu_idxs = None, cores = 1, chains = 4, seed = None, num_w
         sampling_method=SGLD,
         optimizer_kwargs=dict(lr=4e-4, localization=100.0),
         num_chains=chains,  # How many independent chains to run
-        num_draws=200,  # How many samples to draw per chain
+        num_draws=10,  # How many samples to draw per chain
         num_burnin_steps=0,  # How many samples to discard at the beginning of each chain
         num_steps_bw_draws=1,  # How many steps to take between each sample
         device=device,
@@ -55,57 +55,75 @@ def get_stats(device, gpu_idxs = None, cores = 1, chains = 4, seed = None, num_w
         seed=seed,
     )
 
-def distance(s1, s2):
+def check(s1, s2, rtol=1e-3, reverse = False):
+    """
+    Check if two stats are close to each other.
+    
+    """
     assert s1.keys() == s2.keys(), f"Expected the same keys in both stats, got {s1.keys()} and {s2.keys()}."
     assert s1["llc/trace"].shape == s2["llc/trace"].shape, f"Expected the same shape for llc/trace, got {s1['llc/trace'].shape} and {s2['llc/trace'].shape}."
-    return np.mean((s1["llc/trace"] - s2["llc/trace"])**2)
+    valid = np.allclose(s1["llc/trace"], s2["llc/trace"], rtol=rtol)
+    if reverse:
+        valid = not valid
+    assert valid, f"Expected {'different' if reverse else 'close'} llc/trace in both stats, got {s1['llc/trace']} and {s2['llc/trace']}."
 
 @pytest.fixture(scope="module")
 def cpu_default():
     return get_stats("cpu", seed = 100)
 
+@pytest.mark.gpu
 @pytest.fixture(scope="module")
 def gpu_default():
     return get_stats("cuda", seed = 100)
 
 def test_cpu_consistent(cpu_default):
     repeat_stats = get_stats("cpu", seed=100)
-    assert distance(cpu_default, repeat_stats) < 2e-3, f"Repeat trials with the same seed on the CPU were not consistent. MSE: {distance(cpu_default, repeat_stats)}."
+    check(cpu_default, repeat_stats, 1e-3)
 
 def test_cpu_consistent_seeds(cpu_default):
     diff_seed_stats = get_stats("cpu", seed=101)
-    assert distance(cpu_default, diff_seed_stats) > 1, f"Trials with different seeds on the CPU were consistent. MSE: {distance(cpu_default, diff_seed_stats)}."
+    check(cpu_default, diff_seed_stats, 0.1, reverse = True)
 
 def test_cpu_multicore(cpu_default):
     multicore_stats = get_stats("cpu", seed=100, cores = 4)
-    assert distance(cpu_default, multicore_stats) < 2e-3, f"Multicore trials with the same seed on the CPU were not consistent. MSE: {distance(cpu_default, multicore_stats)}."
+    check(cpu_default, multicore_stats, 1e-4)
 
 def test_cpu_multiworker(cpu_default):
     multiworker_stats = get_stats("cpu", seed=100, num_workers = 4)
-    assert distance(cpu_default, multiworker_stats) < 2e-3, f"Multiworker trials with the same seed on the CPU were not consistent. MSE: {distance(cpu_default, multiworker_stats)}."
+    check(cpu_default, multiworker_stats, 1e-4)
 
+@pytest.mark.gpu
 def test_gpu_consistent(gpu_default):
     repeat_stats = get_stats("cuda", seed=100)
-    assert distance(gpu_default, repeat_stats) < 2e-3, f"Repeat trials with the same seed on the GPU were not consistent. MSE: {distance(gpu_default, repeat_stats)}."
+    check(gpu_default, repeat_stats, 5e-3)
 
+@pytest.mark.gpu
 def test_gpu_consistent_seeds(gpu_default):
     diff_seed_stats = get_stats("cuda", seed=101)
-    assert distance(gpu_default, diff_seed_stats) > 1, f"Trials with different seeds on the GPU were consistent. MSE: {distance(gpu_default, diff_seed_stats)}."
+    check(gpu_default, diff_seed_stats, 0.1, reverse = True)
 
+@pytest.mark.gpu
 def test_gpu_multicore(gpu_default):
     multicore_stats = get_stats("cuda", seed=100, cores = 4)
-    assert distance(gpu_default, multicore_stats) < 2e-3, f"Multicore trials with the same seed on the GPU were not consistent. MSE: {distance(gpu_default, multicore_stats)}."
+    check(gpu_default, multicore_stats, 5e-3)
 
+@pytest.mark.gpu
 def test_gpu_multiworker(gpu_default):
     multiworker_stats = get_stats("cuda", seed=100, num_workers = 4)
-    assert distance(gpu_default, multiworker_stats) < 2e-3, f"Multiworker trials with the same seed on the GPU were not consistent. MSE: {distance(gpu_default, multiworker_stats)}."
+    check(gpu_default, multiworker_stats, 5e-3)
 
+@pytest.mark.gpu
 def test_multigpu(gpu_default):
     if torch.cuda.device_count() > 1:
         multigpu_stats = get_stats("cuda", seed=100, gpu_idxs = [0, 1], cores = 2)
-        assert distance(gpu_default, multigpu_stats) < 2e-3, f"Multigpu trials with the same seed on the GPU were not consistent. MSE: {distance(gpu_default, multigpu_stats)}."
-        
+        check(gpu_default, multigpu_stats, 5e-3)
+    else:
+        pytest.skip("Multiple GPUs unavailable.")
+
+@pytest.mark.gpu
 def test_multigpu_multicore(gpu_default):
     if torch.cuda.device_count() > 1:
         multigpu_multicore_stats = get_stats("cuda", seed=100, gpu_idxs = [0, 1], cores = 4)
-        assert distance(gpu_default, multigpu_multicore_stats) < 2e-3, f"Multigpu multicore trials with the same seed on the GPU were not consistent. MSE: {distance(gpu_default, multigpu_multicore_stats)}."
+        check(gpu_default, multigpu_multicore_stats, 5e-3)
+    else:
+        pytest.skip("Multiple GPUs unavailable.")
